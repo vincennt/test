6a26d43f8bfd219c7a093aef30c74de2c0eae495
Vincent <vins@Pimentas-MacBook-Air.local>
2021-12-01 14:37:46 +0100

    exo finish

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const showAlert = () => {
+    alert("Hi im an alert");
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.text1}>
           Open up App.js to start working on your app!
@@ -15,9 +27,25 @@ export default function App() {
         <Text style={styles.text3}>
           Open up App.js to start working on your app!
         </Text>
+        <Image
+          source={{
+            uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png",
+          }}
+          style={styles.img}
+        />
+        <Image
+          style={styles.img}
+          source={require("/Users/vins/Documents/konexio/react/test/assets/star-gd71baaab0_640.jpg")}
+        />
+
+        <Pressable style={styles.ton} onPress={showAlert}>
+          Boutton
+        </Pressable>
+        <ActivityIndicator color="white" />
+
         <StatusBar style="auto" />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -28,7 +56,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   main: {
-    backgroundColor: "white",
+    backgroundColor: "grey",
   },
   text2: {
     margin: 80,
@@ -41,5 +69,12 @@ const styles = StyleSheet.create({
   text3: {
     margin: 80,
     fontWeight: "900",
+  },
+  img: {
+    width: 300,
+    height: 100,
+  },
+  ton: {
+    backgroundColor: "grey",
   },
 });
