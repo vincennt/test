03f0ece559cc4d5d5e45535d941739c42488aaed
Vincent <vins@Pimentas-MacBook-Air.local>
2021-12-01 13:58:19 +0100

    1 done

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.main}>
+        <Text style={styles.text1}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text style={styles.text2}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text style={styles.text3}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -14,8 +24,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+    padding: 10,
+  },
+  main: {
+    backgroundColor: "white",
+  },
+  text2: {
+    margin: 80,
+    textAlign: "center",
+  },
+  text1: {
+    fontSize: 30,
+    margin: 80,
+  },
+  text3: {
+    margin: 80,
+    fontWeight: "900",
   },
 });
